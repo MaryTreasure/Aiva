c320f7d02ef637eb2fc8477757253e004454d4b6
MaryTreasure <mari1991918@gmail.com>
2023-01-22 13:42:13 +0300

    Сделала обратную связь и футер

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ if (iconMenu) {
     const menuBody = document.querySelector('.menu__body');
     const iconMenuHidden = document.querySelector('.menu__icon--hover');
     iconMenu.addEventListener("click", function(e) {
-        document.body.classList.toggle('_lock');
         iconMenu.classList.toggle('_active');
         menuBody.classList.toggle('_active');
         if (iconMenuHidden.classList.contains('menu__icon--hover')) {
