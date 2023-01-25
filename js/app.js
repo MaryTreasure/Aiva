8319e439cbe0db47bfea61b8696261a4fca2671a
MaryTreasure <mari1991918@gmail.com>
2023-01-25 20:11:41 +0300

    Добавила скролл элементов

--- js/app.js
+++ js/app.js
@@ -69,3 +69,24 @@ new Swiper('.image-slider',{
         },
     }
 });
+
+// анимация при скролле
+
+function onEntry(entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+
+            change.target.classList.add('element-show');
+        }
+    })
+}
+
+let options = {
+    threshold: [0.5] };
+    let observer = new IntersectionObserver(onEntry, options);
+    let elements = document.querySelectorAll('.element-animation');
+
+    for (let elm of elements) {
+        observer.observe(elm);
+    }
+
